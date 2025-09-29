1f8d2b985ae9f943c68cdbcb3b58801a0c9d0e1c
Vivek <tvivek966@gmail.com>
2025-09-29 15:03:11 +0530

    Updated allowed origins

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="MySQL Database API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://sql-query-frontend-br5n.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://sql-query-frontend-br5n-viveks-projects-07cdf7ae.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
